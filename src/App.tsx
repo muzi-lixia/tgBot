c1d15f3ddf3ae67b96846a19fb0210ffe2c525b9
muzi_lixia <muzi_lixia@163.com>
2024-07-24 15:02:48 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+import WebApp from '@twa-dev/sdk'
+
+function App() {
+
+    WebApp.setBackgroundColor('#000000')
+
+    WebApp.MainButton.setParams({
+        text: 'Main Button'
+    });
+    WebApp.MainButton.onClick(function () {
+        WebApp.showAlert('Main Button was clicked')
+    });
+    WebApp.MainButton.show();
+
+    function toggleMainButton() {
+        if (WebApp.MainButton.isVisible) {
+            WebApp.MainButton.hide();
+        } else {
+            WebApp.MainButton.show();
+        }
+    }
+
+    return (
+        <>
+            <button onClick={toggleMainButton}>Toggle Main Button</button>
+        </>
+    )
+}
+
+export default App
