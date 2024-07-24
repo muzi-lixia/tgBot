a10ff051faeca0791800ec4f55ca599bb0d32a63
muzi_lixia <muzi_lixia@163.com>
2024-07-24 17:10:30 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import WebApp from '@twa-dev/sdk'
 import eruda from 'eruda'
+import { useEffect, useState } from 'react';
 
 function App() {
 
@@ -22,11 +23,22 @@ function App() {
         }
     }
 
-    console.log(WebApp.initData);
+    const [userInfo, setUserInfo] = useState<any>()
+    
+    useEffect(() => {
+        const info = WebApp.initDataUnsafe
+        console.log(WebApp.initData);
+        console.log(WebApp.initDataUnsafe);
+        console.log(info);
+        if (info) {
+            setUserInfo(info)
+        }
+    }, [])
     
 
     return (
         <>
+            <div>{userInfo && JSON.stringify(userInfo)}</div>
             测试
             <button onClick={toggleMainButton}>Toggle Main Button</button>
         </>
