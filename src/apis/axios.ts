1442c50f54fafe66456e9260c6f7f145a295988c
muzi_lixia <muzi_lixia@163.com>
2024-07-25 17:36:11 +0800

    update

---
+++ src/apis/axios.ts
@@ -0,0 +1,36 @@
+import _axios from 'axios'
+
+const instance = _axios.create({
+    baseURL: '',
+    timeout: 30000,
+    headers: {
+        Authorization: 'Bearer merlinadmin'
+    }
+})
+
+// Add a request interceptor
+instance.interceptors.request.use(
+    function (config) {
+        // Do something before request is sent
+        config.headers["Id-Token"] = window.sessionStorage.getItem('jwt')
+        return config
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error)
+    }
+)
+
+// Add a response interceptor
+instance.interceptors.response.use(
+    function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response.data
+    },
+    function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error?.response?.data)
+    }
+)
