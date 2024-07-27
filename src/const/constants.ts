8117463928515f64b7952fd008c0c06b989fbfbe
muzi_lixia <muzi_lixia@163.com>
2024-07-27 19:38:58 +0800

    update

--- src/const/constants.ts
+++ src/const/constants.ts
@@ -1,4 +1,4 @@
 // tg bot地址
-export const BOT_URL = 'https://t.me/TerpollyGameBot/start'
+export const BOT_URL = 'https://t.me/TerpollyGameTestBot/start'
 // share text
 export const SHARE_TEXT = '🤩 Come to play Terpolly checkin-to-earn game! 💵 Earn $0.01 - $1 dollar value stablecoin with every claim! '
