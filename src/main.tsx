41b61e51861d02c1c410099bed6750b495fde3fd
muzi_lixia <muzi_lixia@163.com>
2024-07-29 14:37:35 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,13 @@ import './index.css'
 import WebApp from '@twa-dev/sdk'
 
 WebApp.ready()
-
+try {
+    if (WebApp.initDataUnsafe) {
+        WebApp.expand()
+    }
+} catch (error) {
+    console.log(error)
+}
 setTimeout(() => {
     ReactDOM.createRoot(document.getElementById('root')!).render(
         <React.StrictMode>
