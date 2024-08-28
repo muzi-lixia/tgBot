2d96810e95ca71cf2ba83d92d48924545c3e801e
muzi_lixia <muzi_lixia@163.com>
2024-08-28 20:32:07 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-import dotenv from 'dotenv'
-import path from 'path'
 
-const env = dotenv.config().parsed
-const baseUrl = {
-    dev: './',
-    test: '/terpollyGameBot/',
-    prod: '/terpollyGameBot/'
-}
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     return {
@@ -18,15 +10,6 @@ export default defineConfig(({ mode }) => {
             outDir: './docs',
             chunkSizeWarningLimit: 1600,
             assetsInlineLimit: 8192
-        },
-        base: baseUrl[mode as 'dev' | 'test' | 'prod'],
-        define: {
-            'process.env': env
-        },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src')
-            }
         }
     }
 })
